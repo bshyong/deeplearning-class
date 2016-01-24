da380e289e17a382430fe36fca18a197086cda79
Benjamin Shyong <ben.shyong@gmail.com>
2016-01-23 22:54:29 -0800

    initial commit

---
+++ assignment1.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import tarfile
+import urllib
+from IPython.display import display, Image
+from scipy import ndimage
+from sklearn.linear_model import LogisticRegression
+import cPickle as pickle
