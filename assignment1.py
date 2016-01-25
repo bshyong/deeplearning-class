d70c326d4eff34fa16a50f0af2a76020c0caddd8
Benjamin Shyong <ben.shyong@gmail.com>
2016-01-24 20:42:45 -0800

    problem one, update gitignore

--- assignment1.py
+++ assignment1.py
@@ -44,3 +44,45 @@ def extract(filename):
 
 train_folders = extract(train_filename)
 test_folders = extract(test_filename)
+
+# Problem one
+image_size = 28  # Pixel width and height.
+pixel_depth = 255.0  # Number of levels per pixel.
+
+def load(data_folders, min_num_images, max_num_images):
+  dataset = np.ndarray(
+    shape=(max_num_images, image_size, image_size), dtype=np.float32)
+  labels = np.ndarray(shape=(max_num_images), dtype=np.int32)
+  label_index = 0
+  image_index = 0
+  for folder in data_folders:
+    print(folder)
+    for image in os.listdir(folder):
+      if image_index >= max_num_images:
+        raise Exception('More images than expected: %d >= %d' % (
+          num_images, max_num_images))
+      image_file = os.path.join(folder, image)
+      try:
+        image_data = (ndimage.imread(image_file).astype(float) -
+                      pixel_depth / 2) / pixel_depth
+        if image_data.shape != (image_size, image_size):
+          raise Exception('Unexpected image shape: %s' % str(image_data.shape))
+        dataset[image_index, :, :] = image_data
+        labels[image_index] = label_index
+        image_index += 1
+      except IOError as e:
+        print('Could not read:', image_file, ':', e, '- it\'s ok, skipping.')
+    label_index += 1
+  num_images = image_index
+  dataset = dataset[0:num_images, :, :]
+  labels = labels[0:num_images]
+  if num_images < min_num_images:
+    raise Exception('Many fewer images than expected: %d < %d' % (
+        num_images, min_num_images))
+  print('Full dataset tensor:', dataset.shape)
+  print('Mean:', np.mean(dataset))
+  print('Standard deviation:', np.std(dataset))
+  print('Labels:', labels.shape)
+  return dataset, labels
+train_dataset, train_labels = load(train_folders, 450000, 550000)
+test_dataset, test_labels = load(test_folders, 18000, 20000)
